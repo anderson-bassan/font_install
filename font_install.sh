f08648046f52ce82214469ca325405909b895194
anderson-bassan <bassan.programador@gmail.com>
2020-01-23 18:17:31 -0300

    add logic to verify if the first argument is an existing .ttf file

--- font_install.sh
+++ font_install.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
 if [ $1 ];then
-	echo "seartch by $1 font"
+	if [  -f "$1" ] && [ $(echo $1  | grep -P "(.ttf)") ] ;then
+		echo "search by the file"
+	
+	else
+		echo "the first command argument must an existing .ttf file, a folder containing .ttf files or a zip file"
+	
+	fi
 
 else
 	echo "ask user for font name"
