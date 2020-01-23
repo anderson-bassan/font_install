d5b4140e0fb56a38cb99367e24f57b8d03edc9ae
anderson-bassan <bassan.programador@gmail.com>
2020-01-23 18:47:46 -0300

    move logic to verify if the first argument is valid to a separeted function

--- font_install.sh
+++ font_install.sh
@@ -1,20 +1,30 @@
 #! /bin/bash
 
-if [ $1 ];then
+function isValidInput {
 	if [  -f "$1" ] && [ $(echo $1  | grep -P "(.ttf)") ] ;then
 		echo "search by the file"
+		return 0
 	
 	elif [  -d "$1" ] && [ "$(ls $1  | grep -P "(.ttf)")" ] ;then
 		echo "valid folder"
-	
+		return 0
 	
 	elif [  -f "$1" ] && [ "$(echo $1  | grep -P "(.zip)")" ] && [ "$(unzip -l $1  | grep -P "(.ttf)")" ] ;then
 		echo "valid zip"
+		return 0
 	
 	else
 		echo "Error: the first command argument must an existing .ttf file, a folder containing .ttf files or a zip file"
+		return 1
 	
 	fi
+
+}
+
+
+if [ $1 ];then
+	isValidInput $1
+	echo $?
 
 else
 	echo "ask user for font name"
