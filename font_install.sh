3a3b7d5b34b083b331b937aaf07cf77715d54061
anderson-bassan <bassan.programador@gmail.com>
2020-01-23 18:26:32 -0300

    add logic to verify if the first argument is an existing folder containing .ttf files

--- font_install.sh
+++ font_install.sh
@@ -4,6 +4,9 @@ if [ $1 ];then
 	if [  -f "$1" ] && [ $(echo $1  | grep -P "(.ttf)") ] ;then
 		echo "search by the file"
 	
+	elif [  -d "$1" ] && [ "$(ls $1  | grep -P "(.ttf)")" ] ;then
+		echo "valid folder"
+	
 	else
 		echo "the first command argument must an existing .ttf file, a folder containing .ttf files or a zip file"
 	
